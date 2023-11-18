51d311dca0b6243c2e2dc5fb52c5119b9ffe0086
Jacob Zimmerman <jacob.zimmerman135@gmail.com>
2023-11-18 13:36:32 -0500

    renamed some stuff

--- person.proxy.cpp
+++ person.proxy.cpp
@@ -4,30 +4,30 @@ using namespace std;
 #include "person.idl"
 #include "common/basis.h"
 #include "common/mem.h"
-#define SOCKET (*RPCPROXYSOCKET)
+#define RPCSOCKET (*RPCPROXYSOCKET)
 #include "common/transmit.h"
 
 void __pack_Person(Person bandhi, __rpcmem_t *mem) {
 		__pack_string(bandhi.name, mem);
 		__pack_int(bandhi.age, mem);
-		for (int i = 0; i < 5; i++) {
-		    __pack_int(bandhi.lucky_numbers[i], mem);
+		for (int __i = 0; __i < 5; __i++) {
+		    __pack_int(bandhi.lucky_numbers[__i], mem);
 		}
 }
 
 void __pack_People(People bandhi, __rpcmem_t *mem) {
-		for (int i = 0; i < 4; i++) {
-		    __pack_Person(bandhi.people[i], mem);
+		for (int __i = 0; __i < 4; __i++) {
+		    __pack_Person(bandhi.people[__i], mem);
 		}
-		for (int i = 0; i < 4; i++) {
-		    __pack_string(bandhi.nicknames[i], mem);
+		for (int __i = 0; __i < 4; __i++) {
+		    __pack_string(bandhi.nicknames[__i], mem);
 		}
 }
 
 Person __unpack_Person(__rpcmem_t *mem) {
     Person result;
-		for (int i = 5; i > 0; i--) {
-		    result.lucky_numbers[i - 1] = __unpack_int(mem);
+		for (int __i = 5; __i > 0; __i--) {
+		    result.lucky_numbers[__i - 1] = __unpack_int(mem);
 		}
 		result.age = __unpack_int(mem);
 		result.name = __unpack_string(mem);
@@ -36,11 +36,11 @@ Person __unpack_Person(__rpcmem_t *mem) {
 
 People __unpack_People(__rpcmem_t *mem) {
     People result;
-		for (int i = 4; i > 0; i--) {
-		    result.nicknames[i - 1] = __unpack_string(mem);
+		for (int __i = 4; __i > 0; __i--) {
+		    result.nicknames[__i - 1] = __unpack_string(mem);
 		}
-		for (int i = 4; i > 0; i--) {
-		    result.people[i - 1] = __unpack_Person(mem);
+		for (int __i = 4; __i > 0; __i--) {
+		    result.people[__i - 1] = __unpack_Person(mem);
 		}
     return result;
 }
@@ -49,8 +49,8 @@ float avg_4D(int nums[8][8][8][8]) {
     __rpcmem_t *mem = rpcmem_new();
     char fname[MAX_FNAME_LEN] = "avg_4D";
 
-		for (int i = 0; i < 8 * 8 * 8 * 8; i++) {
-		    __pack_int(nums[0][0][0][i], mem);
+		for (int __i = 0; __i < 8 * 8 * 8 * 8; __i++) {
+		    __pack_int(nums[0][0][0][__i], mem);
 		}
 
     rpc_send(fname, mem);
