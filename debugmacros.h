c74d41f20103e3c1ce6039c48cf9e6bc49089636
Jacob Zimmerman <jacob.zimmerman135@gmail.com>
2023-11-21 23:41:13 -0500

    no clue whats wrong

--- debugmacros.h
+++ debugmacros.h
@@ -9,4 +9,8 @@
 #define DEBUGMEM(m)                                                            \
     fprintf(stderr, "%d::%s HEAP: %d, STACK: %d, SIZE: %d\n", __LINE__,        \
             __FILE__, m->hp, m->sp, m->hp + ((m->capacity) - m->sp))
+
+#define DEBUGBUF(buf, start, end)                                              \
+    for (int __i = start; __i < (int)end; __i++)                               \
+        fprintf(stderr, #buf "[%02d]: %02x\n", __i, buf[__i]);
 #endif
